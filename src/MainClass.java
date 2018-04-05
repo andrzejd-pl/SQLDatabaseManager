29b675344ade61713f78c708b749e9ffddaaaa55
Andrzej Dybowski <dybowski@andrzejd.pl>
2018-04-06 00:22:17 +0200

    test mysql lib

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,5 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.sql.*;
+
 public class MainClass {
     public static void main(String[] args) {
-        System.out.println("Welcome in SQLDatabaseManager!!!");
+        String user, passwd;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        try {
+            System.out.println("Welcome in SQLDatabaseManager!!!");
+            System.out.print("User: ");
+            user = br.readLine();
+            System.out.print("Passwd: ");
+            passwd = br.readLine();
+
+            Connection conn = DriverManager.getConnection("jdbc:mysql://192.168.0.101/database", user, passwd);
+            Statement statement = conn.createStatement();
+            ResultSet resultSet = statement.executeQuery("select * from firmy;");
+
+            while (resultSet.next()) {
+                System.out.println(resultSet.getRow());
+            }
+
+            resultSet.close();
+            statement.close();
+            conn.close();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
